a6a99adfa18c1b5c5cd8e5cf954d772b17eba4db
ioberman <153344306+ioberman@users.noreply.github.com>
2024-03-17 18:40:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import pg from "pg";
 import axios from "axios";
 
 const app = express();
+const port =10000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
